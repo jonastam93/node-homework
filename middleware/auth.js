66797d2625a23d12ecd6332b39c5d4d032eff9b2
jonastam93 <jonathan.stambuk@gmail.com>
2026-07-23 19:38:34 -0400

    deleted the duplicate auth.js file from my repo

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+function auth(req, res, next) {
+    if (!global.user_id) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        });
+    }
+    next();
+}
+
+module.exports = auth;
