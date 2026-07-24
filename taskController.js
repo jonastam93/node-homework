071f800f180011d5cea27987d9e4418d0552cd80
jonastam93 <jonathan.stambuk@gmail.com>
2026-07-23 20:18:52 -0400

    added task ownership

--- taskController.js
+++ taskController.js
@@ -4,4 +4,17 @@ function taskCounter() {
     return currentTaskId++;
 }
 
+const task = {
+    id: taskCounter(),
+    title: req.body.title,
+    isCompleted: false,
+    userId: global.user_id.email, 
+};
+
+global.tasks.push(task);
+
+const { userId, ...sanitizedTask } = task;
+
+return res.status(201).json(sanitizedTask);
+
 module.exports = taskCounter;
