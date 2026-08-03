e9fcf187d242f3aadeb46723b77b052a4df16c5c
jonastam93 <jonathan.stambuk@gmail.com>
2026-08-03 14:54:02 -0400

    modified error handler in order to detect a specific database error when PostgreSQL isn't running

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,7 @@
 function errorHandler(err, req, res, next) {
+    if (err.code === "ECONNREFUSED" && err.port === 5432) {
+        console.log("The database connection was refused. Is your database service running?");
+    }
     console.error(err);
 
     return res.status(500).json({
