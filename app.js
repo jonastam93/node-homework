2d8a9c9371bca7a15604babc776f20191c745078
jonastam93 <jonathan.stambuk@gmail.com>
2026-08-02 20:34:46 -0400

    added graceful shutdown to app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const notFound = require("./middleware/not-found");
 const errorHandler = require("./middleware/error-handler");
 const authMiddleware = require("./middleware/auth");
 const taskRouter = require("./routes/taskRoutes");
+const pool = require("./db/pg-pool");
 
 const app = express();
 
@@ -34,5 +35,20 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`Server is listening on port ${port}...`);
 });
+
+async function shutdown() {
+    console.log('Shutting down...');
+
+    // Stop accepting new connections
+    server.close();
+
+    // Close all PostgreSQL connections
+    await pool.end();
+
+    process.exit(0);
+}
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
 
 module.exports = { app, server };
