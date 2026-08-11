55a2d16de61f7fe9a8e61e6c32ea1ffde00dd9c5
jonastam93 <jonathan.stambuk@gmail.com>
2026-08-11 14:21:51 -0400

    updated index() to check tasks.length after findMany()

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -50,6 +50,12 @@ async function index(req, res, next) {
       },
     });
 
+    if (tasks.length === 0) {
+      return res.status(404).json({
+        message: "No tasks found.",
+      });
+    }
+
     return res.status(200).json(tasks);
   } catch (error) {
     return next(error);
