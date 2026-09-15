ef1da8105c11967eee38a10f85720bdb3c940976
jonastam93 <jonathan.stambuk@gmail.com>
2026-09-14 23:50:58 -0400

    added jwtMiddleware to logoff route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
+
 const userController = require("../controllers/userController");
+const jwtMiddleware = require("../middleware/jwtMiddleware");
 
 const router = express.Router();
 
 router.post("/register", userController.register);
 router.post("/logon", userController.logon);
-router.post("/logoff", userController.logoff);
+router.post("/logoff", jwtMiddleware, userController.logoff);
 router.post("/googleLogon", userController.googleLogon);
 
 module.exports = router;
